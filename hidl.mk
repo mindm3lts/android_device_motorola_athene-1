6b4fc7c757b79e8ace5a50f2487f8975668d6a30
Lyudmilla17 <merkel.naller@gmail.com>
2018-10-27 23:55:30 +0200

    athene: Use a custom camera service

--- hidl.mk
+++ hidl.mk
@@ -13,7 +13,12 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_moto \
     camera.device@3.2-impl
+
+# Configstore
+PRODUCT_PACKAGES += \
+	android.hardware.configstore@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
